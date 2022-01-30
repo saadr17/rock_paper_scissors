e116d21a193332800d5805b3120e3c68fe721a6e
saadr17 <annihilator24@gmail.com>
2022-01-29 22:05:11 -0600

    add buttons for player choice

--- script.js
+++ script.js
@@ -1,51 +1,81 @@
-let choices =['rock', 'paper', 'scissors']
+let choices = ['rock', 'paper', 'scissors']
 
 const computerPlay = (choices) => {
-const random = Math.floor(Math.random()*choices.length)
+  const random = Math.floor(Math.random() * choices.length)
   let computerChoice = choices[random]
-return computerChoice
-    
+  return computerChoice
+
 };
+
 
 const playerSelection = () => {
-  let playerChoice = parseInt(prompt('pick rock(0) paper(1) or scissors(2)'));
-return choices[playerChoice]
+  
 };
 
+  
+let playerChoice ='';
 
-// console.log(
-// computerPlay(choices)
-// )
+let rockButton = document.querySelector('#rockButton')
+let paperButton = document.querySelector('#paperButton')
+let scissorsButton = document.querySelector('#scissorsButton')
 
-// console.log(
-//     playerSelection()
-// )
+rockButton.addEventListener('click', () => {
+  playerChoice = (choices[0])
+  console.log(playerChoice)
+  
+  // alert('This is the rock button')
+})
+paperButton.addEventListener('click', () => {
+  playerChoice = (choices[1])
+  console.log(playerChoice)
+})
+scissorsButton.addEventListener('click', () => {
+  playerChoice = (choices[2])
+  console.log(playerChoice)
+})
+
+
+  
+while(playerChoice === ''){
+  let begin = document.querySelector('#beginMessage')
+  begin.innerHTML = 'PICK AN OPTION TO BEGIN THE GAME'
+  
+}
+
+
+
+
 
 const game = (computerChoice, playerChoice) => {
-  if(computerChoice === 'rock' && playerChoice === 'scissors'){
-      return `computer chose ${computerChoice}, which beats ${playerChoice}, computer wins`
-  }else if(computerChoice === 'paper' && playerChoice === 'rock'){
-      return `computer chose ${computerChoice}, which beats ${playerChoice}, computer wins`
-  }else if(computerChoice === 'scissors' && playerChoice === 'paper'){
-      return `computer chose ${computerChoice}, which beats ${playerChoice}, computer wins`
+  let computerScore = 0;
+  let userScore = 0;
+
+  
+  
+  if (computerChoice === 'rock' && playerChoice === 'scissors') {
+    return `computer chose ${computerChoice}, which beats ${playerChoice}, computer wins`
+  } else if (computerChoice === 'paper' && playerChoice === 'rock') {
+    return `computer chose ${computerChoice}, which beats ${playerChoice}, computer wins`
+  } else if (computerChoice === 'scissors' && playerChoice === 'paper') {
+    return `computer chose ${computerChoice}, which beats ${playerChoice}, computer wins`
   }
-// player wiining scenarios
-  else if(computerChoice === 'rock' && playerChoice === 'paper'){
-return `player chose ${playerChoice}, which beats ${computerChoice}, player wins`
-  }else if(computerChoice === 'paper' && playerChoice === 'scissors'){
+  // player wiining scenarios
+  else if (computerChoice === 'rock' && playerChoice === 'paper') {
     return `player chose ${playerChoice}, which beats ${computerChoice}, player wins`
-      }
-      else if(computerChoice === 'scissors' && playerChoice === 'rock'){
-          return `player chose ${playerChoice}, which beats ${computerChoice}, player wins`
-      }
-      else{
-          return `draw player chose ${playerChoice} and computer chose ${computerChoice}`
-      }
+  } else if (computerChoice === 'paper' && playerChoice === 'scissors') {
+    return `player chose ${playerChoice}, which beats ${computerChoice}, player wins`
+  }
+  else if (computerChoice === 'scissors' && playerChoice === 'rock') {
+    return `player chose ${playerChoice}, which beats ${computerChoice}, player wins`
+  }
+  else {
+    return `draw player chose ${playerChoice} and computer chose ${computerChoice}`
+  }
 };
 
 //play game 5 times
 
-for(let i =0; i <5; i++){
-    console.log(game(computerPlay(choices), playerSelection()))
-}
+// for(let i =0; i <5; i++){
+//     console.log(game(computerPlay(choices), playerChoice))
+// }
 
